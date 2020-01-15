d7e7b19effcdb5a95c01543ea1eb4b6608356f44
wadeeakush <wadeeabukush@gmail.com>
2020-01-15 17:13:50 +0200

    Adding Contacts us page

---
+++ Group20Project/Group20Project/ContactUsPage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ContactUsPage.aspx.cs" Inherits="Dyslaxia.ContactUsPage" %>
+<link href="StyleSheet.css" rel="stylesheet" />
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Contact Us</h1>
+	
+    <asp:TextBox ID="ContactUsEmail" runat="server" placeholder="Email" TextMode="Email"></asp:TextBox>
+    <asp:TextBox ID="ContactUsMassege" runat="server" placeholder="Massege" Height="100px"></asp:TextBox>
+		<div>
+			
+			&nbsp;<asp:Button ID="ContactUsSendButton" runat="server" Text="Send" OnCommand="ContactUsSendButton_Command" />
+
+		</div>
+    </form>
+</body>
+</html>
---
+++ Group20Project/Group20Project/ContactUsPage.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Dyslaxia
+{
+    public partial class ContactUsPage : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ContactUsSendButton_Command(object sender, CommandEventArgs e)
+        {
+            string dat = "Insert into [ContactUs](Email,Message) Values('" + ContactUsEmail.Text + "','" + ContactUsMassege.Text + "')";
+            SqlCommand comm = new SqlCommand(dat, con);
+            con.Open();
+            comm.ExecuteNonQuery();
+            con.Close();
+            Response.Redirect("MainPage.aspx");
+        }
+    }
+}
